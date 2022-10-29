6cfda3c91012c632c1282ad77ac3d3b79dbfa496
Patrick Marnane <pamarnane@gmail.com>
2022-10-29 23:30:54 +0100

    Implemented search functionality on image gallery

--- app/src/main/java/org/wit/mygolftracker/activities/ImageGallery.kt
+++ app/src/main/java/org/wit/mygolftracker/activities/ImageGallery.kt
@@ -5,23 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.mygolftracker.R
 import org.wit.mygolftracker.adapters.ImageGalleryAdapter
 import org.wit.mygolftracker.adapters.ImageGalleryListener
-
-import org.wit.mygolftracker.databinding.ActivityGolfRoundListBinding
 import org.wit.mygolftracker.databinding.ActivityImageGalleryBinding
-import org.wit.mygolftracker.helpers.showImagePicker
 import org.wit.mygolftracker.main.MainApp
 import org.wit.mygolftracker.models.GolfRoundModel
+import timber.log.Timber.i
 
 class ImageGallery : AppCompatActivity(), ImageGalleryListener {
     lateinit var app : MainApp
     lateinit var binding: ActivityImageGalleryBinding
+    lateinit var searchView: SearchView
     private lateinit var refreshIntentLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,12 +30,24 @@ class ImageGallery : AppCompatActivity(), ImageGalleryListener {
         setContentView(binding.root)
 
         app = application as MainApp
-
-        //val layoutManager = LinearLayoutManager(this)
+        searchView = binding.imageSearch
         val layoutManager = GridLayoutManager(this, 3)
         binding.recyclerImageView.layoutManager = layoutManager
-        //binding.recyclerImageView.adapter = ImageGalleryAdapter(app.golfRounds.findAll(),this)
         loadImages()
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+               // i("$query")
+                filterImages(query)
+                return false
+            }
+
+            override fun onQueryTextChange(msg: String): Boolean {
+               // i("$msg")
+                filterImages(msg)
+                return false
+            }
+        })
 
         registerRefreshCallback()
     }
@@ -58,6 +69,14 @@ class ImageGallery : AppCompatActivity(), ImageGalleryListener {
 
     fun showImages (golfRounds: List<GolfRoundModel>) {
         binding.recyclerImageView.adapter = ImageGalleryAdapter(golfRounds, this)
+        binding.recyclerImageView.adapter?.notifyDataSetChanged()
+    }
+
+    fun filterImages (query: String?) {
+        val golfRounds = app.golfRounds.findAllwImages()
+
+        val filteredGolfRounds = golfRounds.filter { it.course.lowercase().contains("$query".lowercase()) }
+        binding.recyclerImageView.adapter = ImageGalleryAdapter(filteredGolfRounds, this)
         binding.recyclerImageView.adapter?.notifyDataSetChanged()
     }
 
